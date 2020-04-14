39540abde38b717cc8def93c88b832343333f258
grace-mugoiri <gracemugoiri@gmail.com>
2020-04-14 10:58:09 +0300

    add routes

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,2 +1,42 @@
 class Api::V1::AppointmentsController < ApplicationController
+	def index
+		@appointments = Appointment.all
+		render json: @appointments
+	end
+
+	def show
+		render json: @appointment
+	end
+
+	def create
+		@appointment = Appointment.new(appoint_params)
+		 if @appointment.save
+			render json: @appointment
+		 else
+			render error: { error: 'Unable to create'}, status: 400
+		 end
+	end
+
+	def update
+		if @appointment
+			@appointment.update(appoint_params)
+			render json: {message: 'Success'}, status: 200
+		else
+			render json: {error: 'Unable to update Appointment!'}, status: 400
+		end
+	end
+
+	def destroy
+		if @appointment
+			@appointment.destroy
+			render json: {message: 'Deleted'}, status 200
+		else
+			render json: {error: 'Unable to delete !'}, status 400
+		end
+	end
+
+	def appoint_params
+		params.require(:appointment).permit(:date, :time, :instructor, :exercise)
+	end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,48 @@
 class Api::V1::UsersController < ApplicationController
+
+	def index
+		@users = User.all
+		render json: @users
+	end
+
+	def show
+		@user = User.find(params[:id])
+		render json: @user
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			render json: @user
+		else
+			render error: {error: 'Unable to create User!'}, status: 400
+		end
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user
+			@user.update(user_params)
+			render json: {message: 'Success'}, status: 200
+		else
+			render json: {error: 'Unable to update User!'}, status: 400
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		if @user
+			@user.destroy
+			render json: {message: 'Deleted'}, status 200
+		else
+			render json: {error: 'Unable to delete User!'}, status 400
+		end
+	end
+
+	private
+
+	def user_params
+		paramsrequire(:user).permit(:name, :phoneNumber)
+	end
+	
 end
